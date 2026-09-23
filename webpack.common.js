f347680a89f37454db99fab5b4249f713c0398a2
dharrison37-gitit <308913916+dharrison37-gitit@users.noreply.github.com>
2026-09-23 01:46:07 -0500

    Update webpack config files

--- webpack.common.js
+++ webpack.common.js
@@ -1,15 +1,23 @@
 import path from "node:path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
 
 export default {
-    entry: {
-        app: "./src/index.js",
+  entry: {
+    app: "./src/index.js",
+  },
+  output: {
+    filename: "index.js",
+    path: path.resolve(import.meta.dirname, "dist"),
+    clean: true,
+    html: {
+      title: "Production",
     },
-    output: {
-        filename: "[name].bundle.js",
-        path: path.resolve(import.meta.dirname, "dist"),
-        clean: true,
-        html: {
-            title: "Production",
-        },
-    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+      filename: "index.html",
+      inject: true,
+    }),
+  ],
 };
