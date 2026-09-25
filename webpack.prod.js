c431df19ad86cf667556ab0836ad49ea508e98ab
dharrison37-gitit <308913916+dharrison37-gitit@users.noreply.github.com>
2026-09-25 17:39:51 -0500

    Update webpack prod config

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,5 +2,21 @@ import { merge } from "webpack-merge";
 import common from "./webpack.common.js";
 
 export default merge(common, {
-    mode: "production",
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.html$/i,
+        use: ["html-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)/i,
+        use: "asset/resource",
+      },
+    ],
+  },
 });
